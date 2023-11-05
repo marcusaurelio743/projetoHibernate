e7a56ddfdb5fad3331bacc6115aa44095fc701f9
marcus aurelio <marcusaurelio743@gmail.com>
2023-11-04 23:20:46 -0300

    finalizando formulario de cadastro com jsf e adicionando metodo de alterar e deletar e mostrando mensagem nos campos

--- projetohibernate/src/main/java/managedBean/UsuarioManagedBean.java
+++ projetohibernate/src/main/java/managedBean/UsuarioManagedBean.java
@@ -1,7 +1,12 @@
 package managedBean;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import Dao.DaoGeneric;
 import Model.Usuario;
@@ -13,6 +18,7 @@ public class UsuarioManagedBean {
 	private Usuario usuario = new Usuario();
 	
 	private DaoGeneric<Usuario> daoGeneric = new DaoGeneric<Usuario>();
+	private List<Usuario> list = new ArrayList<Usuario>();
 
 	public Usuario getUsuario() {
 		return usuario;
@@ -23,13 +29,28 @@ public class UsuarioManagedBean {
 	}
 	
 	public String salvar() {
-		 daoGeneric.salvar(usuario);
+		 daoGeneric.salvar(usuario); 
+		 this.novo();
+		 FacesContext.getCurrentInstance().
+		 	addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Informação","Salvo com sucesso!!!"));
 		 	
 	return "";
 	}
 	
 	public String novo() {
 		usuario = new Usuario();
+		
+		return "";
+	}
+	public List<Usuario> getList() {
+		list = daoGeneric.listar(Usuario.class);
+		return list;
+	}
+	
+	public String deletar() {
+		
+		daoGeneric.deletarPorId(usuario);
+		this.novo();
 		
 		return "";
 	}
